ad417275cf8dc6c04bce86d4c77d659b580ef36b
PuetzD <puetz-daniela@web.de>
2026-09-04 13:25:24 +0200

    chunk

--- admin-web/vite.config.ts
+++ admin-web/vite.config.ts
@@ -15,9 +15,6 @@ export default defineConfig({
           if (id.includes('/node_modules/react-admin/') || id.includes('/node_modules/ra-')) {
             return 'react-admin'
           }
-          if (id.includes('/node_modules/@mui/')) {
-            return 'mui'
-          }
           if (id.includes('/node_modules/react-router') || id.includes('/node_modules/@remix-run/')) {
             return 'router'
           }
