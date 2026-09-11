26322c82acfd02b213027342d3334fba1173abd6
PuetzD <puetz-daniela@web.de>
2026-09-11 09:50:19 +0200

    fix migrations

--- src/main/resources/db/migration/V9__schedule_outbox_retries.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE integration_outbox
-    ADD COLUMN next_attempt_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    ADD COLUMN quarantined_at TIMESTAMPTZ;
-
-CREATE INDEX ix_integration_outbox_eligible
-    ON integration_outbox (next_attempt_at, created_at, event_id)
-    WHERE published_at IS NULL AND quarantined_at IS NULL;
